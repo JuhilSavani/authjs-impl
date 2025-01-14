c082f2c42051283dbe3b4807ecf5afb318a89534
JuhilSavani <savanijuhil@gmail.com>
2025-01-14 14:13:16 +0530

    Configure form response provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import FormResponseProvider from "@/lib/providers/FormResponseProvider";
 
 export const metadata: Metadata = {
   title: "Authjs Impl",
@@ -12,7 +13,9 @@ export default function RootLayout(
   return (
     <html lang="en">
       <body className={`antialiased`}>
+      <FormResponseProvider>
         {children}
+      </FormResponseProvider>
       </body>
     </html>
   );
