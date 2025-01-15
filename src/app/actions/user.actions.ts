a3335d35769982f42c59d8c32eb09f3a1ea4a866
JuhilSavani <savanijuhil@gmail.com>
2025-01-15 11:07:49 +0530

    Add the actions to fetch user data from the database

--- src/app/actions/user.actions.ts
+++ src/app/actions/user.actions.ts
@@ -1,11 +1,31 @@
 "use server"
 
-// TODO: complete the implementation of these actions
+import { db } from '@/db/index'; 
+import { users } from '@/db/models'; 
+import { eq } from 'drizzle-orm';
 
 export async function getUserById(id: string){
-  console.log(`called getUserById(${id})`);
+  try {
+    const result = await db.select().from(users).where(eq(users.id, id));
+    if (!result || result.length === 0) {
+      return { ok: false, message: 'Account is not available over the provided details', user: null };
+    }
+    return { ok: true, message: 'Accoun found successfully' , user: result[0] };
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return { ok: false, message: 'An error occurred while fetching the account detail', user: null };
+  }
 }
 
 export async function getUserByEmail(email: string){
-  console.log(`called getUserByEmail(${email})`);
+  try {
+    const result = await db.select().from(users).where(eq(users.email, email));
+    if (!result || result.length === 0) {
+      return { ok: false, message: 'User not found', user: null };
+    }
+    return { ok: true, message: 'Account found successfully', user: result[0] };
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return { ok: false, message: 'An error occurred while fetching the account detail', user: email };
+  }
 }
