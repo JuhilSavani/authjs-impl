00710bd5c550ef247e69b4137d92883ed8085e96
JuhilSavani <savanijuhil@gmail.com>
2025-01-16 23:08:50 +0530

    Add oauth providers

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,21 @@
-import Credentials from "next-auth/providers/credentials"
-import { CredentialsSignin, type NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import GitHub from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import { CredentialsSignin, type NextAuthConfig } from "next-auth";
 import { SignInSchema } from '@/lib/schemas'
 import { getUserByEmail } from "@/app/actions/user.actions";
 import bcrypt from 'bcryptjs';
 
 export default { 
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const { email, password } = await SignInSchema.parseAsync(credentials);
@@ -16,7 +26,7 @@ export default {
         if(!getUserResult.user!.isVerified)
           throw new CredentialsSignin("Please verify your email to continue further!");
 
-        if(getUserResult.user!.provider !== "credentials") 
+        if(getUserResult.user!.provider !== "credentials" && !getUserResult.user.password) 
           throw new CredentialsSignin(`Please verify your email or try to sign in with ${getUserResult.user!.provider}!`);
 
         const isMatch = await bcrypt.compare(password, getUserResult.user.password as string);
