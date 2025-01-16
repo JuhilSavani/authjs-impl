eb90947654a3d956a7787231dc80850c282469f2
JuhilSavani <savanijuhil@gmail.com>
2025-01-16 23:11:03 +0530

    Add action to save the users signed with oauth providers

--- src/app/actions/user.actions.ts
+++ src/app/actions/user.actions.ts
@@ -4,13 +4,35 @@ const baseUrl = `http://localhost:3000`;
 
 export async function getUserByEmail(email: string){
   try {
-    const result = await fetch(`${baseUrl}/api/user/${email}`);
-    const { message, user } = await result.json();
-    if (result.ok) return { ok: true, message, user };
+    const response = await fetch(`${baseUrl}/api/user/${email}`);
+    const { message, user } = await response.json();
+    if (response.ok) return { ok: true, message, user };
     return { ok: false, message, user };
   } catch (error) {
     console.error('[user.actions][Error fetching user by email]:', error);
     return { ok: false, message: error instanceof Error ? error.message : 'An error occurred while fetching the account detail', user: null };
+  }
+}
+
+export async function saveUserDetails(data: { 
+  firstName: string, 
+  lastName: string, 
+  email: string, 
+  isVerified: boolean, 
+  provider: "google" | "github" 
+}) {
+  try {
+    const response = await fetch(`${baseUrl}/api/user`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+    const { message } = await response.json();
+    if (response.ok) return { ok: true, message };
+    return { ok: false, message };
+  } catch (error) {
+    console.error('[user.actions][Error saving user details]:', error);
+    return { ok: false, message: error instanceof Error ? error.message : 'An error occurred while saving the user details' };
   }
 }
 
